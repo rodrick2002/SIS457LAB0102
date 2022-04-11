77495877275debd64faa57c8d0c97e88a1cf6b9a
rodrick2002 <rodrigo02.llawar@gmail.com>
2022-04-11 18:11:13 -0400

    se agrega el proyecto

---
+++ SIS457LAB0102.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int n;
+
+int main()
+{
+    do {
+
+        cout << "Hola que tal\n";
+        cout << "Mi nombre es Rodrigo Llawar Condori Avendaño\n";
+        cout << "Naci el 4 de diciembre del año 2002 en sucre en el hospital de lajastambo  \n";
+        cout << "Actualmente tengo 19 años, vivo con mis padres y mis dos hermanos  \n";
+        cout << "Estudie en el colegio San vicente de Paul...y actualmente estudio en la USFX\n";
+        cout << "Trabajo atendiendo una cancha de futbol\n";
+        cout << "Me encanta dibujar , es mi pasatiempo \n";
+        cout << "Me gusta el futbol el voley y el box, ademas de jugar videojuegos\n";
+        cout << "Eso fue un resumen de mi vida\n";
+        cout << "Si quieres verlo de nuevo pulsa 1 y si no 0\n";
+        cin >> n;
+
+    } while (n == 1);
+
+
+}
